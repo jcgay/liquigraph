4dda4ea7be4851a2bc1ecf55a8c3b78f7ffa3096
Florent Biville <florent.biville@gmail.com>
2015-05-16 15:50:18 +0200

    [#40] Separate tx for execution and history insert

--- liquigraph-core/src/test/java/org/liquigraph/core/api/LiquigraphTest.java
+++ liquigraph-core/src/test/java/org/liquigraph/core/api/LiquigraphTest.java
@@ -1,9 +1,11 @@
 package org.liquigraph.core.api;
 
-import org.liquigraph.core.configuration.ConfigurationBuilder;
-import org.liquigraph.core.rules.EmbeddedGraphDatabaseRule;
+import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
+import org.liquigraph.core.configuration.ConfigurationBuilder;
+import org.liquigraph.core.rules.EmbeddedGraphDatabaseRule;
+import org.neo4j.kernel.api.exceptions.schema.UniqueConstraintViolationKernelException;
 
 import java.sql.*;
 
@@ -19,11 +21,11 @@ public class LiquigraphTest {
     @Test
     public void runs_migrations_against_embedded_graph_with_failed_precondition_whose_changeset_is_marked_as_executed() throws SQLException {
         liquigraph.runMigrations(
-                new ConfigurationBuilder()
-                        .withRunMode()
-                        .withMasterChangelogLocation("changelog-with-1-node.xml")
-                        .withUri(graph.uri())
-                        .build()
+            new ConfigurationBuilder()
+                .withRunMode()
+                .withMasterChangelogLocation("changelog-with-1-node.xml")
+                .withUri(graph.uri())
+                .build()
         );
 
         try (Connection connection = graph.jdbcConnection()) {
@@ -52,6 +54,46 @@ public class LiquigraphTest {
                 assertThat(resultSet.next()).isFalse();
                 connection.commit();
             }
+        }
+    }
+
+    @Test
+    public void runs_migrations_with_schema_changes() throws SQLException {
+        liquigraph.runMigrations(
+            new ConfigurationBuilder()
+                .withRunMode()
+                .withMasterChangelogLocation("schema/schema-changelog.xml")
+                .withUri(graph.uri())
+                .build()
+        );
+
+        try (Connection connection = graph.jdbcConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("MATCH (foo:Foo {bar: 123}) RETURN foo")) {
+
+            assertThat(resultSet.next()).isTrue();
+            assertThat(resultSet.next()).isFalse();
+            connection.commit();
+        }
+    }
+
+    @Test
+    public void runs_migrations_with_schema_changes_and_preconditions() throws SQLException {
+        liquigraph.runMigrations(
+            new ConfigurationBuilder()
+                .withRunMode()
+                .withMasterChangelogLocation("schema/schema-preconditions-changelog.xml")
+                .withUri(graph.uri())
+                .build()
+        );
+
+        try (Connection connection = graph.jdbcConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("MATCH (foo:Foo {bar: 123}) RETURN foo")) {
+
+            assertThat(resultSet.next()).isTrue();
+            assertThat(resultSet.next()).isFalse();
+            connection.commit();
         }
     }
 
